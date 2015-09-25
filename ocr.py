24ad7ad97e91df56220a570f5a64d5906378678c
Malcolm Gin <malcolm.gin@gmail.com>
2015-09-25 09:26:39 -0700

    Starting kata.

--- ocr.py
+++ ocr.py
@@ -2,9 +2,26 @@
 
 Bank OCR kata implements a thing that reads and validates example OCR numbers."""
 
+class OcrNumeral:
+  """OcrNumeral tracks each digit in an OCR application."""
+  asciiValue = ""
+  numeralValue = -1
+  alternateValues = []
+  
+  def __init__(self, asciiVal, numVal, altVals):
+    self.asciiValue = asciiVal
+    self.numeralValue = numVal
+    self.alternateValues = altVals
+
+class OcrNumeralParser:
+  """OcrNumeralParser works in conjunction with OcrNumeral() class to parse and assign the right numerals to an OCR account string."""
+  pass
+
+
 class BankOcr:
   """BankOcr class to fulfill OCR Kata.
 
   Assumptions:
   """
+  pass
 
--- testClass.py
+++ testClass.py
@@ -1,5 +1,13 @@
 from nose.tools import assert_is, assert_is_instance, assert_in, assert_equal, raises
 from ocr import *
 
-class TestOCR:
+class TestOcrNumeral:
 
+  def testOcrNumeralInstantiationAndInit(self):
+    ocrString = ( "   " 
+                  "|_|" 
+                  "  |" )
+    ocrNum = OcrNumeral(ocrString, 4, [9])
+    assert_equal(ocrString, ocrNum.asciiValue)
+    assert_equal(4, ocrNum.numeralValue)
+    assert_equal([9], ocrNum.alternateValues)
