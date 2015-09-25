1ff6b4ec93370af7e3a9aac0c2b991a34d77a2bd
Malcolm Gin <malcolm.gin@gmail.com>
2015-09-24 22:13:17 -0700

    Initial setup of class module and testing module.

---
+++ ocr.py
@@ -0,0 +1,10 @@
+"""Module to implement Bank OCR Kata. See README.md for more information.
+
+Bank OCR kata implements a thing that reads and validates example OCR numbers."""
+
+class BankOcr:
+  """BankOcr class to fulfill OCR Kata.
+
+  Assumptions:
+  """
+
---
+++ testClass.py
@@ -0,0 +1,5 @@
+from nose.tools import assert_is, assert_is_instance, assert_in, assert_equal, raises
+from ocr import *
+
+class TestOCR:
+
